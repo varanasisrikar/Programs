ee4fd190845b7152a2deedcafc0f54cb29bcb51d
varanasisrikar <varanasisrikar@gmail.com>
2019-09-18 21:58:05 +0530

    Code

--- Python/Greatest_Number_3_Numbers.py
+++ Python/Greatest_Number_3_Numbers.py
@@ -1,11 +1,11 @@
-#The program to find the greatest of three numbers
-n1=int(input('Enter the first number:'))
-n2=int(input('Enter the second number:'))
-n3=int(input('Enter the third number:'))
-if(n1>n2):
-    if(n1>n3):
-        print(n1 , "is greater")
-    elif(n2>n3):
-       print(n2, "is greater")
+# The program to find the greatest of three numbers
+n1 = int(input('Enter the first number:'))
+n2 = int(input('Enter the second number:'))
+n3 = int(input('Enter the third number:'))
+if(n1 > n2):
+    if(n1 > n3):
+        print(n1, "is greater")
+    elif(n2 > n3):
+        print(n2, "is greater")
 else:
-       print(n3,  "is greater")
+    print(n3,  "is greater")
--- Python/Prime_Check.py
+++ Python/Prime_Check.py
@@ -1,9 +1,9 @@
-n=int(input("Enter the Number:"))
-if n>1:
-    for i in range(2,n):
-        if(n%i)==0:
-            print(n,"is not a Prime Number")
-            print(i,"times",n//i,"is",n)
+n = int(input("Enter the Number:"))
+if n > 1:
+    for i in range(2, n):
+        if(n % i) == 0:
+            print(n, "is not a Prime Number")
+            print(i, "times", n // i, "is", n)
             break
     else:
-        print(n,"is a Prime Number")
+        print(n, "is a Prime Number")
