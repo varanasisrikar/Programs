c0a3d66430edeb2fa3d2589603a3b3a47e8ea65b
varanasisrikar <varanasisrikar@gmail.com>
2019-09-18 21:27:56 +0530

    Code

--- Python/Amount_Withdrawal.py
+++ Python/Amount_Withdrawal.py
@@ -1,5 +1,5 @@
-Amount=int(input("Enter Amount"))
-if(Amount>1000):
+Amount = int(input("Enter Amount"))
+if(Amount > 1000):
     print("Insufficient Funds")
 else:
     print("Amount can be withdrawn")
