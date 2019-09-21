6c251009aff8bda46884e4dd51d9e78a96a22af5
varanasisrikar <varanasisrikar@gmail.com>
2019-09-21 11:31:40 +0530

    Code

--- DS/linkedlistdelete.c
+++ DS/linkedlistdelete.c
@@ -3,81 +3,81 @@
 
 struct num
 {
-    int n;
-    struct num *next;
+        int n;
+        struct num *next;
 };
 
 
 int main(void)
 {
-    struct num *head = NULL;
-    // create a node
-    while(1)
-    {
-        int x = 0;
-        struct num *ptr = (struct num*) malloc (sizeof(struct num));
-        if (ptr == NULL)
-        {
-            return 1;
-        }
-        printf("Enter a no. ");
-        scanf("%d", &x);
-        if (x == EOF)
-        {
-            break;
-        }
-        ptr->n = x;
-        ptr->next = NULL;
+        struct num *head = NULL;
 
-        if (head == NULL)
+        while(1)
         {
-            head = ptr;
+                int x = 0;
+                struct num *ptr = (struct num*) malloc (sizeof(struct num));
+                if (ptr == NULL)
+                {
+                        return 1;
+                }
+                printf("Enter a no. ");
+                scanf("%d", &x);
+                if (x == EOF)
+                {
+                        break;
+                }
+                ptr->n = x;
+                ptr->next = NULL;
+
+                if (head == NULL)
+                {
+                        head = ptr;
+                }
+                else
+                {
+                        for (struct num *nptr = head; nptr != NULL; nptr=nptr->next)
+                        {
+                                if (nptr->next == NULL)
+                                {
+                                        nptr->next = ptr;
+                                        break;
+                                }
+                        }
+                }
+
+        }
+
+
+        int d = 0;
+        printf("Enter the node to be deleted: ");
+        scanf("%d", &d);
+
+        if (d == 1)
+        {
+                struct num *dptr = head;
+                head = dptr->next;
+                dptr->next = NULL;
+                free(dptr);
         }
         else
         {
-            for (struct num *nptr = head; nptr != NULL; nptr=nptr->next)
-            {
-                if (nptr->next == NULL)
+                int i = 1;
+                for (struct num *dptr = head; dptr != NULL; dptr = dptr->next)
                 {
-                    nptr->next = ptr;
-                    break;
+                        if (i == d-1)
+                        {
+                                struct num * deletee = dptr->next;
+                                dptr->next = dptr->next->next;
+                                deletee->next  = NULL;
+                                free(deletee);
+                        }
+                        i++;
                 }
-            }
         }
 
-    }
-    //delete a node
-
-    int d = 0;
-    printf("Enter the node to be deleted: ");
-    scanf("%d", &d);
-
-    if (d == 1)
-    {
-        struct num *dptr = head; // dptr points to head
-        head = dptr->next; // head points to 2nd node
-        dptr->next = NULL; // node to be deleted points to NULL
-        free(dptr);
-    }
-    else
-    {
-        int i = 1;
-        for (struct num *dptr = head; dptr != NULL; dptr = dptr->next)
+        for (struct num *disptr = head; disptr != NULL; disptr = disptr->next)
         {
-            if (i == d-1)
-            {
-                struct num * deletee = dptr->next;
-                dptr->next = dptr->next->next;
-                deletee->next  = NULL;
-                free(deletee);
-            }
-            i++;
+                printf("\nNode: %d\n", disptr->n);
         }
-    }
-// print the linked list:
-    for (struct num *disptr = head; disptr != NULL; disptr = disptr->next)
-    {
-        printf("\nNode: %d\n", disptr->n);
-    }
-    return 0;
+        return 0;
 }
