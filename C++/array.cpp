33123effc31fd4e1e608f1f2a1c6c1007bfe5332
varanasisrikar <varanasisrikar@gmail.com>
2019-10-01 20:20:34 +0530

    Code

--- C++/array.cpp
+++ C++/array.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 int main()
 {
-   int array[5],i;
-   cout<<"Enter elements: ";
-   for(i=0;i<5;i++)
-   {
-       cin>>array[i];
-   }
-   cout<<"You entered: "<<endl;
-   for(i=0;i<5;i++)
-   {
-       cout<<array[i]<<endl;
-   }
+        int array[5],i;
+        cout<<"Enter elements: ";
+        for(i=0; i<5; i++)
+        {
+                cin>>array[i];
+        }
+        cout<<"You entered: "<<endl;
+        for(i=0; i<5; i++)
+        {
+                cout<<array[i]<<endl;
+        }
 }
