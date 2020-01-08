05549b25afc7b38b1b27bca4ad9d9bd67550ec3f
varanasisrikar <varanasisrikar@gmail.com>
2020-01-08 21:29:20 +0530

    Code

--- SQL/TableCreate.sql
+++ SQL/TableCreate.sql
@@ -1,13 +1,12 @@
-drop database db;
 create database db;
 use db;
-create table empc(empno integer(4) primary key,
+create table emp(
+  empno integer(4) primary key,
   ename varchar(50) not null,
   job varchar(50) not null,
   mgr int(4),
   hiredate date,
-  sal decimal(10,2),
-  omm decimal(10,2),
+  sal decimal(10, 2),
   deptno integer(2)
 );
-desc empc;
+desc emp;
