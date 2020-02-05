0429024ed75363e79845ba9ba93bcd36186393be
varanasisrikar <varanasisrikar@gmail.com>
2020-02-05 11:49:11 +0530

    Code

--- Java/Consecutivenumberreversepattern.java
+++ Java/Consecutivenumberreversepattern.java
@@ -1,26 +1,5 @@
-/*
-write a java  program  to print the following pattern for any given positive integer?
-
-for other boundary conditions print -1.
-
-case = 1
-input = 4
-output =
-1
-2 1
-3 2 1
-4 3 2 1
-
-case = 2
-input = 3
-output =
-1
-2 1
-3 2 1
-
-*/
 import java.util.Scanner;
-class Consecutivereversepattern
+class Consecutivenumberreversepattern
 {
     public static void main(String[] args)
     {
