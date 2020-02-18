1973530f3538ee8f5db5bf402f11c373ee41480c
varanasisrikar <varanasisrikar@gmail.com>
2020-02-18 08:41:40 +0530

    Code

---
+++ Java/Fibonacci.java
@@ -0,0 +1,34 @@
+import java.util.Scanner; 
+public class Fibonacci { 
+    
+    public static void main(String[] args) { 
+	 Scanner sc = new Scanner(System.in);
+	 int n=sc.nextInt(); 
+        System.out.println(fib(n)); 
+    } 
+ static int fib(int n) {
+     int a=0;
+int b=1;
+int c=0;
+int i=2;
+if(n==0)
+{
+return a;
+}
+if(n==1)
+{
+return b;
+}
+else
+{
+while(i<=n)
+{
+  c=a+b;
+  a=b;
+  b=c;
+  i++;
+}
+return c;
+}
+ }
+ }
