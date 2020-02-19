16ff5953673fd30909b4e709f8c1f784c881a954
varanasisrikar <varanasisrikar@gmail.com>
2020-02-19 12:48:54 +0530

    Code

---
+++ Java/Armstrongno.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.lang.Math;
+class Armstrongno
+{
+    public static void main(String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int temp=n;
+        int sum=0;
+        while(n>0)
+        {
+            int rem = n%10;
+            n = n/10;
+            sum+=Math.pow(rem,3);
+        }
+        if(sum==temp)
+        {
+            System.out.println("it is an Armstrong number.");
+        }
+        else
+        {
+            System.out.println("it is not an Armstrong number.");
+        }
+    }
+}
