7adcd770c327d49500b2114f6f77e01a89414f55
varanasisrikar <varanasisrikar@gmail.com>
2020-02-20 09:05:55 +0530

    Code

---
+++ Java/Comparelexico.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class Comparelexico 
+{
+public static void main(String[] args)
+    {
+         Scanner s = new Scanner(System.in);
+         String st = s.nextLine();
+         System.out.println("Enter first String");
+         System.out.println("Enter second String");
+         String str = s.nextLine();
+         int res = st.compareTo(str);
+         if(res>0)
+         {
+             System.out.println(st+" is greater than "+str);
+         }
+         if(res<0)
+         {
+             System.out.println(st+" is less than "+str);
+         }
+         else if(res==0)
+         {
+             System.out.println(st+" is equals to "+str);
+         }
+    }
+}
