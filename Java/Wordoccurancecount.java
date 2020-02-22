6271e666a9ae9930a2d1a14284a87001da3906a2
varanasisrikar <varanasisrikar@gmail.com>
2020-02-22 19:00:13 +0530

    Code

---
+++ Java/Wordoccurancecount.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Wordoccurancecount
+{
+    public static void main(String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter string and substring");
+        String st = s.nextLine();
+        String str = s.nextLine();
+        int count = 0;
+        String[] sta = st.split(" ");
+        for(int i=0;i<sta.length;i++)
+        {
+            if(str.equals(sta[i]))
+            {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+    
+}
