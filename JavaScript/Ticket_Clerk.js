f08d08a8e93ad866c74f3fc6882e3acd9d896a46
varanasisrikar <varanasisrikar@gmail.com>
2020-03-15 22:16:17 +0530

    Code

--- JavaScript/Ticket_Clerk.js
+++ JavaScript/Ticket_Clerk.js
@@ -1,19 +1,23 @@
 function tickets(peopleInLine) {
   var money = 0;
   var ticket_cost = 25;
+  var sold = 0;
   for (var i = 0; i < peopleInLine.length; i++) {
-    if (peopleInLine.length == 0) {
-      money += ticket_cost;
-    } else if (i == 0) {
-      money += ticket_cost;
-    } else if (peopleInLine[i] > ticket_cost && money + ticket_cost - peopleInLine[i] >= peopleInLine[i] - ticket_cost) {
-      money += ticket_cost;
-      money -= peopleInLine[i] - ticket_cost;
-      return "YES";
-    } else{
-      return "NO";
+    var change = peopleInLine[i] - ticket_cost;
+    money += ticket_cost;
+    if (peopleInLine.length == 1) {
+      sold++;
+    } else if (peopleInLine[i] > ticket_cost && money >= change) {
+      sold++;
+    } else if (peopleInLine[i] == ticket_cost) {
+      sold++;
     }
   }
+  if (sold == peopleInLine.length) {
+    return "YES";
+  } else {
+    return "NO";
+  }
 }
-var x = tickets([25, 25, 50, 50]);
+var x = tickets([25, 100]);
 console.log(x);
